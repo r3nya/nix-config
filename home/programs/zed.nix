152541ee53c281f4a850c99f0cd3fde4fdd20cf6
Andrew Makarov <me@r3nya.ru>
2026-03-08 16:22:41 +0100

    setup zed

--- home/programs/zed.nix
+++ home/programs/zed.nix
@@ -2,17 +2,49 @@
 
 {
   home.file.".config/zed/settings.json".text = builtins.toJSON {
+    autosave = "on_focus_change";
+    auto_install_extensions = {
+      dockerfile = true;
+      html = true;
+      log = true;
+      make = true;
+      sql = true;
+      toml = true;
+    };
+
+    git_panel = {
+      tree_view = true;
+    };
+
     vim = {
+      cursor_shape = {
+        normal = "hollow";
+        insert = "bar";
+        replace = "block";
+        visual = "underline";
+      };
       toggle_relative_line_numbers = true;
+      use_system_clipboard = "on_yank";
     };
 
     minimap = {
       show = "never";
     };
 
+    telemetry = {
+      metrics = false;
+    };
+
+    terminal = {
+      dock = "right";
+    };
+
     vim_mode = true;
     ui_font_size = 16;
-    buffer_font_size = 15;
+    buffer_font_family = "Iosevka Term";
+    buffer_font_size = 16.0;
+    show_whitespaces = "all";
+    wrap_guides = [ 80 ];
 
     theme = {
       mode = "system";
